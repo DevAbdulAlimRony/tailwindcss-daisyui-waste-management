e1be66703df502f92c799cd55390d61e4bd49514
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2025-03-14 21:10:27 +0600

    initialized

---
+++ tailwind.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                main: '#FFDC26',
+                secondary: '#F9DF8A',
+                light: '#FFF9F4',
+                para: '#111111',
+            },
+            fontFamily: {
+                title: ['ADLaM Display', 'sans-serif'],
+                para: ['Sora', 'sans-serif'],
+            }
+
+        }
+    }
+}
